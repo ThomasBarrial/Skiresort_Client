876bbc7411a09d5342144bfe08d0e841547da460
Thomas <barrialthomas@gmail.com>
2021-07-21 14:36:49 +0200

    initconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import Diagram from "./pages/Diagram";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/'>
-          <Diagram/>
-        </Route>
+        <Route exact path="/"></Route>
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
